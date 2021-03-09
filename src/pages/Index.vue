6102b223ec1e19df79dd30ee320cab306db4444b
Rafi992 <rafal.bochniewicz@gmail.com>
2021-03-09 16:09:44 +0100

    Add settings

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,6 +2,7 @@
   <Layout>
     <div v-for="edge in $page.posts.edges" :key="edge.node.id">
       <g-link :to="'blog/' + edge.node.slug">
+        <h1>Blog website</h1>
         <h2>{{ edge.node.title }}</h2>
       </g-link>
       <p>{{ edge.node.description }}</p>
