0717720af88b35ceadc46006016382440b904b88
Rafi992 <rafal.bochniewicz@gmail.com>
2021-03-09 13:37:51 +0100

    first commit

---
+++ gridsome.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  templates: {
+    // Add templates for content types here.
+    // Read more: https://gridsome.org/docs/templates/
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-contentful',
+      options: {
+        space: 'dd9zc6ksxqgv', // required
+        accessToken: 'iX6aZlthHci4T7HiU2jsNlKw6Iz9aSw3DQOj9wTMjwk', // required
+        typeName: 'Contentful',
+      },
+    },
+  ],
+};
---
+++ src/pages/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout>
+    <div v-for="edge in $page.posts.edges" :key="edge.node.id">
+      <h2>{{ edge.node.title }}</h2>
+      <p>{{ edge.node.description }}</p>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query {
+  posts: allContentfulBlogPost {
+    edges {
+      node {
+        title
+        description
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {};
+</script>
